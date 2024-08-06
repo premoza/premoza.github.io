22e82510879795e6ac56e0ef9ced9782e909e0e9
Melvin Jones Repol <mrepol742@gmail.com>
2024-08-06 11:59:21 +0800

    ive no idea what im doin

--- README.md
+++ README.md
@@ -1,40 +1,49 @@
 # premoza
 
 ## Pre-requisites
-- Install Node.js
-- Install PHP Laravel
-
-## Getting started
-- fork the repository https://github.com/premoza/premoza
-- clone the fork repository
-  ```
-  _> using https
-  git clone https://github.com/<your-username>/premoza
-
-  _> using ssh
-  git clone git@github.com:<your-username>/premoza
-  ```
+- XAMPP or mysql cli
+- Node.js v20.^
+- PHP v8.2.^
+- Laravel v11.^ & Composer
+  
+## Setting up
 - install dependecies
   ```
-  composer install
-  npm install
+  composer install && npm install
   ```
+- create environment
+  ```
+  cp .env.example .env
+  ```
+- generate app key
+  ```
+  php artisan key:generate
+  ```
+- launch xampp & start mysql
+  > or use the mysql cmd line interface
 - database migration
   ```
-  php artisan migrate:refresh
+  php artisan migrate
+  php artisan db:seed
   ```
 
-## Run
-- start vite first
+## Start application
+- start vite
   ```
   npm run dev
   ```
 - start laravel
-  ```
+  ```sh
   php artisan serve
   ```
 
-## Squashing migration
+## Refresh migration
 ```
-  php artisan schema:dump --prune
+  php artisan migrate:refresh
+  php artisan db:seed
+```
+
+## Refresh cache
+```
+  php artisan config:cache
 ```
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,81 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="/images/premoza-icon-min.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+            Premoza
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            {{-- Left Side --}}
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+
+            {{-- Right Side --}}
+            <ul class="navbar-nav ms-auto mx-3">
+
+                @guest
+                @if (Route::has('login'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @endif
+
+                @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+                @endif
+                @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+                @endguest
+            </ul>
+
+            <form class="d-flex" role="search">
+                <div class="input-group">
+                    <input type="search" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="search-addon">
+                    <button class="btn btn-outline-primary" id="search-addon" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+            </form>
+        </div>
+    </div>
+</nav>
