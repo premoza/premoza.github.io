2b28afe2e89d547b936783defcde0be0539d78b6
Elisha Okai <46462325+Elitexv@users.noreply.github.com>
2024-10-13 22:56:13 +0100

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import "./Hero.css";
+import CountUp from "react-countup";
+import { motion } from "framer-motion";
+import SearchBar from "../SearchBar/SearchBar";
+import LaunchlistForm from "../LaunchWaitList";
+
+const Hero = () => {
+  return (
+    <section className="hero-wrapper">
+      <div className="paddings  innerWidth flexCenter hero-container">
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <div className="orange-circle" />
+            <motion.h1
+              initial={{ y: "2rem", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 2,
+                type: "ease-in",
+              }}
+            >
+              Blockchain Powered<br />
+          
+              Real estate e-commerce platform.
+              <br /> Property
+            </motion.h1>
+          </div>
+          <div className="flexColStart secondaryText flexhero-des">
+            <span>A cutting-edge real estate e-commerce platform, powered by Solana</span>
+            <span> allowing users to buy, rent, or sell properties and book hotels worldwide with a single click.</span>
+          </div>
+         
+          <LaunchlistForm/>
+          <div className="flexCenter stats">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={8800} end={9000} duration={4} /> <span>+</span>
+              </span>
+              <span className="secondaryText">Premium Product</span>
+            </div>
+
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={1950} end={2000} duration={4} /> <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customer</span>
+            </div>
+
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp end={28} /> <span>+</span>
+              </span>
+              <span className="secondaryText">Awards Winning</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="flexCenter hero-right">
+          <motion.div
+            initial={{ x: "7rem", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              type: "ease-in",
+            }}
+            className="image-container"
+          >
+            <img src="./heroc.png" alt="houses" />
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
