6031816df44bc82d15f04867cf77c635af80c1bf
Melvin Jones Repol <mrepol742@gmail.com>
2024-10-14 07:38:31 +0800

    Initial Commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="f-wrapper">
+      <div className=" paddings innerWidth flexCenter f-container ">
+
+      
+
+        <div className="flexColStart f-right">
+          <span className="primaryText">Socials</span>
+          <span className="secondaryText">145 New York, FL 5467, USA</span>
+          <div className="flexCenter f-menu">
+            <span><a href='https://x.com/Premozaofficial'>Twitter</a></span>
+            <span>Instagram</span>
+            <span></span>
+            <span>About Us</span>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./GetStarted.css";
+
+const GetStarted = () => {
+  return (
+    <div>
+      <div id="getStarted" className="g-wrapper">
+        <div className="paddings innerWidth g-container">
+          <div className="flexColCenter g-innercontainer">
+          <span className="primaryText">Get started with Premoza</span>
+          <span className="secondaryText">Subscribe and find super attractive price quotes from us.
+          <br />
+            Find your residence soon
+          </span>
+          <button className="button">
+            <a href="mailto:madeehawaqas48@gmail.com">Get Started</a>
+          </button>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GetStarted;
